afc214951cf2c7e84b2b6eb3d1fbeb88dbd430a7
Martin Luna <martin.luna@rga.com>
2016-12-26 12:20:22 -0300

    updated slick slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 ( function( $ ) {
 
-  var navigationContainer = $('#site-navigation'),
-    mainNavigation = navigationContainer.find('.c-anchor-nav ul');
-
-  //open or close the menu clicking on the bottom "menu" link
-  $('.menu-toggle').on('click', function(){
-    $(this).toggleClass('menu-is-open');
-    //we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
-    mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
-
+  $('.c-carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    infinite: true,
+    dots: false,
+    arrows: false
   });
 
 } )( jQuery );
--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -8,16 +8,27 @@
     $temp     = $wp_query;
     $wp_query = null;
     $wp_query = new WP_Query();
+    $args = array (
+      'posts_per_page' => -1,
+      'post_type'      => galeria,
+      'page_id'        => 140
+    );
 
-    $wp_query->query('showposts=-1&post_type=galeria&page_id=140');
+    $wp_query->query($args);
 
     while ($wp_query->have_posts()) : $wp_query->the_post();
 
     $images = rwmb_meta( 'embutidos_imgadv', 'type=image&size=full' );
+
     echo "\n<div class=\"c-carousel\">";
+
     foreach ( $images as $image ) {
-      echo "\n<div><img src='{$image['url']}' alt='{$image['alt']}' /></div>";
+
+      $sizes = wp_calculate_image_sizes( array( $image['width'], $image['height'] ), null, null, $image['ID'] );
+
+      echo "\n<div><img src=\"{$image['url']}\" alt=\"{$image['alt']}\" srcset=\"{$image['srcset']}\" sizes=\"{$sizes}\" /></div>";
     }
+
     echo "\n</div>";
 
     endwhile; ?>
