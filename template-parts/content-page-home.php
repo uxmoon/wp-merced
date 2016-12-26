a4289ece0657afb1c4522695efbca99c1fe9c13a
Martin Luna <martin.luna@rga.com>
2016-12-26 14:55:51 -0300

    updated homepage content

--- template-parts/content-page-home.php
+++ template-parts/content-page-home.php
@@ -2,39 +2,56 @@
 
   <div class="entry-content">
 
-
     <?php
 
-    $temp     = $wp_query;
-    $wp_query = null;
-    $wp_query = new WP_Query();
     $args = array (
       'posts_per_page' => -1,
       'post_type'      => galeria,
       'page_id'        => 140
     );
 
-    $wp_query->query($args);
+    query_posts( $args );
 
-    while ($wp_query->have_posts()) : $wp_query->the_post();
+    if ( have_posts() ):
+      while ( have_posts() ) :
+        the_post();
 
-    $images = rwmb_meta( 'embutidos_imgadv', 'type=image&size=full' );
+        // Do stuff with the post content.
+        $images = rwmb_meta( 'embutidos_imgadv', 'type=image&size=full' );
 
-    echo "\n<div class=\"c-carousel\">";
+        echo "\n<div class=\"c-carousel\">";
 
-    foreach ( $images as $image ) {
+        foreach ( $images as $image ) {
 
-      $sizes = wp_calculate_image_sizes( array( $image['width'], $image['height'] ), null, null, $image['ID'] );
+          $sizes = wp_calculate_image_sizes( array( $image['width'], $image['height'] ), null, null, $image['ID'] );
 
-      echo "\n<div><img src=\"{$image['url']}\" alt=\"{$image['alt']}\" srcset=\"{$image['srcset']}\" sizes=\"{$sizes}\" /></div>";
-    }
+          echo "\n<div><img src=\"{$image['url']}\" alt=\"{$image['alt']}\" srcset=\"{$image['srcset']}\" sizes=\"{$sizes}\" /></div>";
+        }
 
-    echo "\n</div>";
+        echo "\n</div>";
 
-    endwhile; ?>
-    <?php $wp_query = null; $wp_query = $temp; ?>
+      endwhile;
+    else:
+        // Insert any content or load a template for no posts found.
+    endif;
 
-		<?php the_content(); ?>
+    wp_reset_query();
+
+    ?>
+
+<!--     <div class="row">
+      <div class="columns">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores illum laboriosam, quod aliquam fugit, optio molestias, odio eum unde delectus assumenda autem. Similique sapiente vel, amet nisi debitis maxime officiis?</p>
+      </div>
+      <div class="columns">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque accusamus, temporibus maxime blanditiis tempora quia facere numquam beatae optio aliquam dolorem magni sunt totam. Incidunt odit consequatur beatae numquam, inventore.</p>
+      </div>
+    </div> -->
+
+    <div class="c-message">
+      <?php the_content(); ?>
+    </div>
+
 	</div><!-- .entry-content -->
 
 </article><!-- #post-## -->
