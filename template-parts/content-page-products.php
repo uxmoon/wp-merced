85d03cb1d3951fe04b8f1812433a8627dcafcf5b
Martin Luna <martin.luna@rga.com>
2016-12-27 15:24:19 -0300

    products page carousel layout updated

--- template-parts/content-page-products.php
+++ template-parts/content-page-products.php
@@ -23,32 +23,39 @@
       while ( have_posts() ) :
         the_post(); ?>
 
-    <?php the_title(); ?>
-    <?php the_content(); ?>
+    <div class="c-products">
+      <div class="c-products__content">
+        <?php the_title('<h2 class="c-products__title">', '</h2>'); ?>
+
+        <?php the_content(); ?>
+      </div>
+      <div class="c-products__image">
+        <?php
+            // Do stuff with the post content.
+            $images = rwmb_meta( 'embutidos_imgadv', 'type=image&size=full' );
+
+            echo "\n<div class=\"c-carousel\">";
+
+            foreach ( $images as $image ) {
+
+              $sizes = wp_calculate_image_sizes( array( $image['width'], $image['height'] ), null, null, $image['ID'] );
+
+              echo "\n<div><img src=\"{$image['url']}\" alt=\"{$image['alt']}\" srcset=\"{$image['srcset']}\" sizes=\"{$sizes}\" /></div>";
+            }
+
+            echo "\n</div>";
+        ?>
+      </div><!-- .c-products__image -->
+    </div>
 
     <?php
-        // Do stuff with the post content.
-        $images = rwmb_meta( 'embutidos_imgadv', 'type=image&size=full' );
-
-        echo "\n<div class=\"c-carousel\">";
-
-        foreach ( $images as $image ) {
-
-          $sizes = wp_calculate_image_sizes( array( $image['width'], $image['height'] ), null, null, $image['ID'] );
-
-          echo "\n<div><img src=\"{$image['url']}\" alt=\"{$image['alt']}\" srcset=\"{$image['srcset']}\" sizes=\"{$sizes}\" /></div>";
-        }
-
-        echo "\n</div>";
-
       endwhile;
     else:
         // Insert any content or load a template for no posts found.
     endif;
 
-    wp_reset_query();
+    wp_reset_query(); ?>
 
-    ?>
 
   </div><!-- .entry-content -->
 
