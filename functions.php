df9bf1d2028f70f3edf7dd15abc48b459a226909
Martin Luna <martin.luna@rga.com>
2016-12-29 15:52:35 -0300

    added cdn js url for slick carousel

--- functions.php
+++ functions.php
@@ -108,13 +108,11 @@ function merced_scripts() {
 
 	wp_enqueue_style( 'quechalen-theme', get_template_directory_uri() . '/css/app.css' );
 
-	wp_enqueue_script( 'merced-slick', get_template_directory_uri() . '/js/slick.min.js', array( 'jquery' ), '20151215', true );
+	wp_enqueue_script( 'merced-slick', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js', array( 'jquery' ), '20151215', true );
 
 	wp_enqueue_script( 'merced-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery' ), '20151215', true );
 
 	wp_enqueue_script( 'merced-modernizr', get_template_directory_uri() . '/js/modernizr.js', array(), '20151215', false );
-
-	// wp_enqueue_script( 'merced-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'merced-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
