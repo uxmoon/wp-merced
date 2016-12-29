af7c58505c15d7d42b5acb357bcf7bfdb94580cf
Martin Luna <martin.luna@rga.com>
2016-12-29 12:22:24 -0300

    updated store and location pages

--- footer.php
+++ footer.php
@@ -14,10 +14,12 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-        <div class="site-info-contact">
-            <span class="site-info-phone">02656.479.268</span>
-            <a href="mailto:info@lamercedembutidos.com.ar">info@lamercedembutidos.com.ar</a>
-        </div>
+    <div class="site-info-contact">
+      <div class="site-info-contact__content">
+        <span class="site-info-phone"><svg class="icon icon-phone"><use xlink:href="#icon-phone"></use></svg> 02656.479.268</span>
+        <a href="mailto:info@lamercedembutidos.com.ar"><svg class="icon icon-envelope"><use xlink:href="#icon-envelope"></use></svg> info@lamercedembutidos.com.ar</a>
+      </div>
+    </div>
 		<div class="site-info">
 			&copy; Copyright <?php echo date('Y'); ?>. Todos los derechos reservados. <?php bloginfo( 'name' ); ?> <?php $description = get_bloginfo( 'description', 'display' ); echo $description; ?>.
 		</div><!-- .site-info -->
