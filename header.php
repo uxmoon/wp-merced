18670b2a572965950c168a94f613fcb97cb8b1ae
Martin Luna <martin.luna@rga.com>
2016-12-29 16:53:00 -0300

    reduce database call for header file

--- header.php
+++ header.php
@@ -10,9 +10,9 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="es-AR">
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
